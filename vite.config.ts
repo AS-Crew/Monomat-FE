c78c93fb9d5d153774933346a586ac419dec494d
hyeon-marina <hyeon.marina@gmail.com>
2026-04-14 02:50:07 +0900

    feat(AS-Crew#2): 코어 스토어 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path' // Node.js 경로 모듈 가져오기
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // @ 기호를 src 폴더의 절대 경로로 매핑
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
